461b28522b96e27e55c76e19237ebd082786712e
Brandon <nbr4nd0n2005@gmail.com>
2025-06-02 21:22:29 +0700

    Fixed delete user button error

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,6 @@
+// rrd imports
+import { Form, NavLink } from "react-router-dom";
+
 // Assets
 import logomark from "../assets/logomark.svg";
 
@@ -7,11 +10,9 @@ import { TrashIcon } from "@heroicons/react/24/solid";
 // Clerk Authentication
 import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/clerk-react";
 
-// rrd imports
-import { Form, NavLink } from "react-router-dom";
-
 const Nav = () => {
   const { isSignedIn, user } = useUser();
+  const userName = localStorage.getItem('userName');
 
   return (
     <nav className="flex items-center justify-between p-4 bg-white shadow">
@@ -39,25 +40,25 @@ const Nav = () => {
           </>
         ) : (
           <div className="flex items-center gap-4">
-            
+            {userName && (
+              <Form
+                method="post"
+                action="/logout" 
+                onSubmit={(event) => {
+                  if (!confirm("Delete user and all data?")) {
+                    event.preventDefault();
+                  } else {
+                    localStorage.removeItem('userName')
+                  }
+                }}
+              >
+                <button type="submit" className="btn btn--warning transform active:scale-95 transition duration-150 ease-in-out">
+                  <span>Delete User</span>
+                  <TrashIcon width={20} />
+                </button>
+              </Form>
+            )}
 
-            {/* Logout Form */}
-            <Form
-              method="post"
-              action="/logout"
-              onSubmit={(event) => {
-                if (!confirm("Delete user and all data?")) {
-                  event.preventDefault();
-                }
-              }}
-            >
-              <button type="submit" className="btn btn--warning transform active:scale-95 transition duration-150 ease-in-out">
-                <span>Delete User</span>
-                <TrashIcon width={20} />
-              </button>
-            </Form>
-
-            {/* Show the Clerk User Button when signed in */}
             <UserButton />
           </div>
         )}
